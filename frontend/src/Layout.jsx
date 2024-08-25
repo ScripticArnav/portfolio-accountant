58f101386f7ba49e65f9fc259e7e4b86efbea7eb
avanisyadav911 <avanish9113@gmail.com>
2024-08-25 21:32:34 +0530

    updated navbar and frontSection

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import {  HeroSection, Navbar } from "./components";
+import {  FrontSection, Navbar } from "./components";
 import {Outlet} from 'react-router-dom'
 const Layout = () => {
   return (
     <>
     <Navbar/>
     <div className='max-w-7xl mx-auto pt-20 px-6'>
-      <HeroSection/>
-    </div>
+      <FrontSection/>
+      </div>
     
     <Outlet/>
     
